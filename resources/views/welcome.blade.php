6d1dd0d739dfafb6d8936c5a76d57fca11120f88
omarfarukrocky <omarfarukrocky19@gmail.com>
2022-04-06 02:33:56 -0700

    new project laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="card">   
+        <div class="card-body">
+            <h4 class="card-title">Home page</h4>       
+        </div>
+    </div>
+</div>
+
+    
+@endsection
